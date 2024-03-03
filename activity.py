11377a2414a47a93e8b39d70ff36bbf47abc91ab
Spandan Barve <spandan567@gmail.com>
2024-03-03 10:48:43 +0000

    Initialize activity scaffolding - main.py, activity.py, activity directory, .flake8

---
+++ activity.py
@@ -0,0 +1,75 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import pygame
+
+from sugar3.activity.activity import Activity
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.graphics.toolbutton import ToolButton
+from sugar3.activity.widgets import StopButton
+
+import sugargame.canvas
+
+from gettext import gettext as _
+from main import GoatCabbageWolf
+
+
+class GoatCabbageWolfActivity(Activity):
+
+    def __init__(self, handle):
+        Activity.__init__(self, handle)
+        self.max_participants = 1
+        self.sound = True
+        self.game = GoatCabbageWolf()
+        self.build_toolbar()
+        self.game.canvas = sugargame.canvas.PygameCanvas(
+                self,
+                main=self.game.run,
+                modules=[pygame.display, pygame.font, pygame.mixer])
+        self.set_canvas(self.game.canvas)
+        self.game.canvas.grab_focus()
+
+    def build_toolbar(self):
+        toolbar_box = ToolbarBox()
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show()
+
+        activity_button = ActivityToolbarButton(self)
+        toolbar_box.toolbar.insert(activity_button, -1)
+        activity_button.show()
+
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(False)
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
+
+        button = ToolButton('speaker-muted-100')
+        button.set_tooltip(_('Sound'))
+        button.connect('clicked', self.sound_control)
+        toolbar_box.toolbar.insert(button, -1)
+
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(True)
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
+
+        stop_button = StopButton(self)
+        toolbar_box.toolbar.insert(stop_button, -1)
+        stop_button.show()
+        stop_button.connect('clicked', self._stop_cb)
+
+    def sound_control(self, button):
+        self.sound = not self.sound
+        self.game.sound = self.sound
+        if not self.sound:
+            button.set_icon_name('speaker-muted-000')
+            button.set_tooltip(_('No sound'))
+        else:
+            button.set_icon_name('speaker-muted-100')
+            button.set_tooltip(_('Sound'))
+
+    def _stop_cb(self, button):
+        self.game.running = False
---
+++ main.py
@@ -0,0 +1,62 @@
+import pygame
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class GoatCabbageWolf:
+    def __init__(self):
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        self.gameDisplay = None
+        self.info = None
+        self.update_function = None
+
+    def vw(self, x):
+        return (x / 100) * self.display_rect.width
+
+    def vh(self, y):
+        return (y / 100) * self.display_rect.height
+
+    def blit_centred(self, surf, x, y):
+        rect = surf.get_rect()
+        centered_coords = (x - rect.width // 2, y - rect.height // 2)
+        self.gameDisplay.blit(surf, centered_coords)
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        self.gameDisplay = pygame.display.get_surface()
+        self.info = pygame.display.Info()
+        self.display_rect = self.gameDisplay.get_rect()
+
+        if not (self.gameDisplay):
+            self.gameDisplay = pygame.display.set_mode(
+                (self.info.current_w, self.info.current_h))
+            pygame.display.set_caption("Goat, Cabbage and Wolf")
+
+        while self.running:
+            self.gameDisplay.fill((2, 20, 20))
+
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    break
+            
+            if self.update_function is not None:
+                self.update_function()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+        return
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+    game = GoatCabbageWolf()
+    game.run()
